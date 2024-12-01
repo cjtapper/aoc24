f03763dc9bbefb4b4aa4840c5417011907cb6606
Chris Tapper <mail@cjtapper.com>
2024-12-01 16:31:27 +1100

    Update to modern

--- support.py
+++ support.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import argparse
 import os.path
-from typing import Callable, TypeAlias
+from typing import TypeAlias
+from collections.abc import Callable
 
 Solver: TypeAlias = Callable[[str], object]
 
